410a4193be129f725c8d1cd0204be1db84af2d97
Shaun Lefeuvre <slefeuvre@mtab.com>
2026-06-10 16:34:47 -0700

    Bump halo to 0.2.25

--- Formula/halo.rb
+++ Formula/halo.rb
@@ -1,9 +1,9 @@
 class Halo < Formula
   desc "CLI for the Halo platform"
   homepage "https://github.com/mTABNPD/halo-platform-cli"
-  version "0.2.24"
-  url "https://github.com/mTABNPD/homebrew-tap/releases/download/v0.2.24/halo-darwin-aarch64.tar.gz"
-  sha256 "28468eabe723437ce3384aaea13eb1b4404358738141f5d56e7d79fa4b3768bb"
+  version "0.2.25"
+  url "https://github.com/mTABNPD/homebrew-tap/releases/download/v0.2.25/halo-darwin-aarch64.tar.gz"
+  sha256 "5462f2f8caf89721aaace6ff974d911eb7cf16cf4e2622310738640354b5e3ae"
 
   depends_on :macos
   depends_on arch: :arm64
